caafb0090825c1bedd28c11491aa49beb484e4f6
oscarmrt <oscarmrt@hotmail.com>
2020-01-05 23:07:46 +0000

    modified pint.c

--- pint.c
+++ pint.c
@@ -17,5 +17,5 @@ void _pint(stack_t **stack, unsigned int line_number)
 		dprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%u\n", tmp->n);
+	printf("%d\n", tmp->n);
 }
