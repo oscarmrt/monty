f840ed8cc28ae3c031d14b528da13d7d6756e3ad
oscarmrt <oscarmrt@hotmail.com>
2020-01-05 23:02:41 +0000

    modified pint.c

--- pint.c
+++ pint.c
@@ -9,13 +9,13 @@
 
 void _pint(stack_t **stack, unsigned int line_number)
 {
-stack_t *tmp;
+	stack_t *tmp;
 
-tmp = *stack;
-if (tmp == NULL)
-{
-	dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
-	exit(EXIT_FAILURE);
-}
-printf("%d\n", tmp->n);
+	tmp = *stack;
+	if (tmp == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
+			exit(EXIT_FAILURE);
+	}
+	printf("%d\n", tmp->n);
 }
