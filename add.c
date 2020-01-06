c5bc87643ceb243dce209d15dec1c7fcfafaf789
oscarmrt <oscarmrt@hotmail.com>
2020-01-06 02:13:25 +0000

    modified main.c

--- add.c
+++ add.c
@@ -17,7 +17,7 @@ void _add(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		dprintf(STDERR_FILENO, "L:%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L:%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
